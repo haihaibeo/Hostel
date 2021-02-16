4583e40d164921fa763f2a85993e891282eed1ad
Hai Pham <repperpep@gmail.com>
2021-02-16 04:29:57 +0300

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import "./App.css";
 import { mode } from "@chakra-ui/theme-tools";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Footer from "./Components/NavComponents/Footer";
 // theme.components.Button.baseStyle.borderRadius = "0";
 
 const myTheme = extendTheme({
@@ -28,16 +29,23 @@ const myTheme = extendTheme({
 export const App = () => (
   <Router>
     <ChakraProvider theme={myTheme}>
-      <Box>
-        {/* <Box textAlign="center" fontSize="xl" pt="4" marginX={["5%", "10%", "15%"]}> */}
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/room/:slug" component={SingleRoom} />
-          <Route exact path="/rooms/:slug" component={RoomsPage} />
-          <Route exact path="/rooms/" component={RoomsPage} />
-          <Route component={ErrorPage} />
-        </Switch>
+
+      <Box d="flex" flexDir="column" minH="100vh">
+        <Box flex="1">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/room/:slug" component={SingleRoom} />
+            <Route exact path="/rooms/:slug" component={RoomsPage} />
+            <Route exact path="/rooms/" component={RoomsPage} />
+            <Route component={ErrorPage} />
+          </Switch>
+        </Box>
+
+        <Box flexShrink={0}>
+          <Footer />
+        </Box>
       </Box>
+
     </ChakraProvider>
   </Router>
 )
---
+++ src/Components/NavComponents/Footer.tsx
@@ -0,0 +1,26 @@
+import { Box, Center, Divider } from '@chakra-ui/react'
+import React from 'react'
+import { BsHeartFill } from 'react-icons/bs';
+
+type FooterProps = {
+    height?: string
+}
+
+const Footer: React.FC<FooterProps> = ({ height }) => {
+    return (
+        <Box mt="10" mb="5">
+            <Divider mb="5" boxShadow="lg" />
+            <Center as="h4">
+                {"Proudly made with"}
+                <>&nbsp;</>
+                <BsHeartFill />
+                <>&nbsp;</>
+                {"by"}
+                <>&nbsp;</>
+                <Box as="a" href="https://github.com/haihaibeo">Hai</Box>
+            </Center>
+        </Box>
+    )
+}
+
+export default Footer;
